c25fdb56fd07734e5d1987fd082813716261f49d
turner harris <turnerharris7798@yahoo.com>
2020-09-02 12:50:49 -0400

    finished exercise

---
+++ BankAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BankEncapsulation;
+
+public class BankAccount
+{
+	private double _balance = 0;
+
+    public void Deposit(double ammount)
+    {
+        _balance += ammount;
+    }
+
+    public double Getbalance()
+    {
+        return _balance;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bank = new BankAccount();
+
+            Console.WriteLine("How much would you like to deposit?: ");
+            double ammountToDeposit = Convert.ToInt32(Console.ReadLine());
+
+            bank.Deposit(ammountToDeposit);
+            double userBalance = bank.Getbalance();
+
+            Console.WriteLine($"Your Balance is : {userBalance}");
+        }
+    }
+}
